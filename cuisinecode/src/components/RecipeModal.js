0053936a04fee9693ca40d3244c91e7d0e548bd9
haelilac <123338828+haelilac@users.noreply.github.com>
2024-01-24 23:10:17 +0800

    Added RecipeModal.js

---
+++ cuisinecode/src/components/RecipeModal.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const RecipeModal = ({ recipe, onClose }) => {
+  const imagePath = process.env.PUBLIC_URL + '/' + recipe.image; // Constructing the image path
+
+  return (
+    <div className="modal-overlay" onClick={onClose}>
+      <div className="modal-content" onClick={(e) => e.stopPropagation()}>
+        <div className="modal-header">
+          <h2>{recipe.name}</h2>
+          <button className="close-button" onClick={onClose}>
+            X
+          </button>
+        </div>
+        <img src={imagePath} alt={recipe.name} />
+        <p>Ingredients: {recipe.ingredients.join(', ')}</p>
+        <p>Procedure: {recipe.procedure}</p>
+      </div>
+    </div>
+  );
+};
+
+export default RecipeModal;
