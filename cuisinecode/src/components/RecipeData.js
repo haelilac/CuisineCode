1673de311dad112ba068389abcf0511a90d057c4
katktrn <130741414+katktrn@users.noreply.github.com>
2024-01-24 10:41:15 +0800

    update recipe.js

---
+++ cuisinecode/src/components/RecipeData.js
@@ -0,0 +1,10 @@
+
+export const recipes = [
+{
+    id: 15,
+    name: "Chicken Inasal",
+    ingredients : ["chicken", "ginger", "lemongrass", "garlic", "vinegar", "lemon or calamansi juice","salt", "brown sugar", "lemon soda", "ground black pepper", "atsuete oil", "margarine"],
+    procedure: "1. In a freezer bag or large bowl, combine 2 lbs chicken cut into serving pieces, lemongrass, salt, ½ tablespoon ground black pepper, 2 tablespoons ginger, 2 tablespoons garlic, ¼ cup of brown sugar, 1 cup vinegar, 1 cup lemon-lime soda, and ½ cup lemon juice.\n2. 2. Stir or shake the mixture until every ingredient is well incorporated. Marinade for 1 to 3 hours.\n3. 3. In a bowl, combine margarine, annatto oil, salt, and lemon juice then stir. Set aside.\n4. 4. Grill the chicken while basting generous amounts of the margarine mixture.\n5. 5. Transfer the grilled chicken to a serving plate.\n6. 6. Serve with sinamak. Share and enjoy!",
+    image: "chicken_inasal.jpg"
+  },
+
