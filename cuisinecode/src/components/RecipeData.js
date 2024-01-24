1e51d0a145cd35bcc8fb070372c61a5c70206716
G_Rodolfo <girondudut92@gmail.com>
2024-01-24 19:32:36 +0800

    Update RecipeDate.Js
    import the recipe code

--- cuisinecode/src/components/RecipeData.js
+++ cuisinecode/src/components/RecipeData.js
@@ -1,16 +1,288 @@
 export const recipes = [
-    {
-      id: 1,
-      name: "Adobo",
-      ingredients: ["chicken", "soy sauce", "vinegar", "garlic"],
-      procedure: "1. Marinate chicken in soy sauce and vinegar.\n 2. Sauté garlic until fragrant.\n 3. Add marinated chicken and simmer until cooked.",
-      image: "images/adobo.jpg"
-    },
-    {
-        id: 2,
-        name: "Chicken Inasal",
-        ingredients: ["chicken", "ginger", "lemongrass", "garlic", "vinegar", "lemon or calamansi juice", "salt", "brown sugar", "lemon soda", "ground black pepper", "atsuete oil", "margarine"],
-        procedure: "1. In a freezer bag or large bowl, combine 2 lbs chicken cut into serving pieces, lemongrass, salt, ½ tablespoon ground black pepper, 2 tablespoons ginger, 2 tablespoons garlic, ¼ cup of brown sugar, 1 cup vinegar, 1 cup lemon-lime soda, and ½ cup lemon juice.\n. 2. Stir or shake the mixture until every ingredient is well incorporated. Marinade for 1 to 3 hours.\n. 3. In a bowl, combine margarine, annatto oil, salt, and lemon juice then stir. Set aside.\n. 4. Grill the chicken while basting generous amounts of the margarine mixture.\n. 5. Transfer the grilled chicken to a serving plate.\n. 6. Serve with sinamak. Share and enjoy!",
-        image: "images/chicken_inasal.jpg"
-      },
+  {
+    id: 1,
+    name: "Pancit Bihon with Lechon",
+    ingredients: [
+      "Bihon",
+      "Knorr Pork Cube",
+      "Pork belly boiled and sliced",
+      "Lechon kawali",
+      "Cooking oil",
+      "Dried shrimp optional",
+      "Cups carrot",
+      "snap peas",
+      "cabbage, chopped",
+      "onion, chopped",
+      "Garlic, chopped",
+      "Chinese sausage, sliced",
+      "oyster sauce",
+      "Soy sauce",
+      "Cups water",
+      "Parsley, chopped",
+      "Ground black pepper to taste" ],
+  
+    procedure: "1. Heat 4 tablespoons of oil in a cooking pot. Saute onion and garlic until the onion softens.\n2. Add 8 ounces of pork belly and sausage. Saute for 3 minutes.\n3. Pour soy sauce and water. Let boil.\n4. Add 2 pieces of Knorr Pork cube. Stir and cook for 1 minute.\n5. Add 24 ounces of bihon noodles. Cover the pot and then continue cooking until the noodles start to soften.\n6. Add 1/2 cup of oyster sauce. Toss until all ingredients are well distributed.\n7. Put in 1/4 cup of dried shrimp, cabbage, snap peas, 1 1/2 carrots. Toss. Cook for 7 minutes.\n8. Season with ground black pepper and 1 cup soy sauce if needed.\n9. Arrange in a serving tray and top with chopped lechon kawali.\n10. Serve with sliced lime or calamansi. Share and enjoy!",
+    image: "Pancit_Bihon_with_lechon.jpg"
+  },
+
+  {
+    id: 2,
+    name: "Pork Adobo",
+    ingredients: [
+      "Pork belly",
+      "Garlic",
+      "Dried bay leaves",
+      "Vinegar",
+      "Soy sauce",
+      "Peppercorn",
+      "Water",
+      "Salt to taste"
+    ],
+    procedure: "1. Combine the 2 lbs pork belly, 1/2 cup soy sauce, and 2 tablespoons garlic then marinade for at least 1 hour.\n2. Heat the pot and put-in the marinated pork belly then cook for a few minutes\n3. Pour remaining marinade including garlic.\n4. Add water, whole pepper corn, and 5 dried bay leaves then bring to a boil. Simmer for 40 minutes to 1 hour\n5. Put in the 4 tablespoons of vinegar and simmer for 12 to 15 minutes\n6. Add salt to taste.\n7. Serve hot. Share and enjoy",
+    image: "Pork_Adobo.jpg"
+  },
+
+  {
+    id: 3,
+    name: "Chicharong Bulaklak",
+    ingredients: [
+      "Ruffle fat",
+      "Whole peppercorn",
+      "Dried bay leaves",
+      "Sea salt",
+      "canola oil",
+      "Water"
+    ],
+    procedure: "1. Arrange the 2 lbs of ruffle fat, 1 tablespoon of whole peppercorn, 2 tablespoons salt, and 5 bay leaves in a cooking pot. Pour-in 6 cups of water. Let boil. Cover and then simmer for 1 hour.\n2. Turn the heat off. Remove the boiled ruffle fat from the pot and then let the water drip-off.\n3. Rub the remaining salt all over the ruffled fat. Set aside.\n4. Meanwhile, heat the 4 cups of cooking oil in a deep fryer or a deep cooking pot.\n5. When the oil gets hot, slowly add-in the ruffled fat. Deep fry in low to medium heat until the texture becomes crisp and the color turns medium brown.\n6. Add salt to taste.\n7. Remove from the deep fryer or cooking pot. Arrange on a plate lined with paper towels to absorb the oil.\n8. Serve with spiced vinegar.",
+    image: "Chicharon_Bulaklak.jpg"
+  },
+  {
+    id: 3,
+    name: "Chicharong_Bulaklak",
+    ingredients: [
+      "Ruffle fat",
+      "Whole peppercorn",
+      "Dried bay leaves",
+      "Sea salt",
+      "4 cups canola oil",
+      "Water"
+    ],
+    procedure: "1. Arrange the 2 lbs of ruffle fat, 1 tablespoon of whole peppercorn, 2 tablespoons salt, and 5 bay leaves in a cooking pot. Pour-in 6 cups of water. Let boil. Cover and then simmer for 1 hour.\n2. Turn the heat off. Remove the boiled ruffle fat from the pot and then let the water drip-off.\n3. Rub the remaining salt all over the ruffled fat. Set aside.\n4. Meanwhile, heat the 4 cups of cooking oil in a deep fryer or a deep cooking pot.\n5. When the oil gets hot, slowly add-in the ruffled fat. Deep fry in low to medium heat until the texture becomes crisp and the color turns medium brown.\n6. Add salt to taste.\n7. Remove from the deep fryer or cooking pot. Arrange on a plate lined with paper towels to absorb the oil.\n8. Serve with spiced vinegar.",
+    image: "Chicharon_Bulaklak.jpg"
+  },
+  
+  {
+    id: 4,
+    name: "Lechon Kawali Sisig",
+    ingredients: [
+      "Lady’s Choice Mayonnaise",
+      "Tablespoons Knorr liquid seasoning",
+      "lemon lime soda",
+      "Calamansi juice",
+      "White vinegar",
+      "Ground black pepper",
+      "Pork belly",
+      "Dried bay leaves",
+      "Whole peppercorn",
+      "Liver spread",
+      "Chili pepper",
+      "Green onions",
+      "Red onions",
+      "Salt",
+      "Water",
+      "Cooking oil"
+    ],
+    procedure: "1. Combine all Sisig seasoning ingredients in a bowl. Mix well until smooth. Set aside.\n2. Make lechon kawali sisig by boiling the pork belly with whole peppercorn and dried bay leaves in 6 to 8 cups of water for 50 minutes or until tender\n3. Remove from the pot and let it cool down.\n4. Rub it with 2 teaspoons of salt. Deep-fry the pork belly until golden brown and crispy.\n5. Chop into serving pieces.\n6. Combine a quarter of the sisig seasoning ingredients with 2 tablespoons of liver spread.\n7. Put the chopped pork belly, Thai chili pepper, green onions, and red onions in a mixing bowl and then add the Sisig seasoning mixture.\n8. Toss until well blended. Serve.",
+    image: "Chicharon_Bulaklak.jpg"
+  },
+  
+  {
+    id: 5,
+    name: "Dynamite Lumpia",
+    ingredients: [
+      "Long green pepper",
+      "Cheddar cheese",
+      "Lumpia wrapper",
+      "Cooking oil",
+      "Ground pork",
+      "Onion minced",
+      "Cloves garlic crushed and minced",
+      "Egg",
+      "Cooking oil",
+      "Salt and ground black pepper to taste"
+    ],
+    procedure: "1. Prepare the ground pork stuffing by heating 3 tablespoons of oil in a pan. Saute garlic and onion until the latter softens. Add ground pork. Saute until medium brown. Season with salt and ground black pepper. Remove from the pan and put on a large bowl. Let it cool down. Beat 1 piece of egg and pour into the cooked ground pork. Mix well. Set aside.\n2. Slice one side of the peppers lengthwise all the way to the bottom. Remove the seeds by gently scraping using a small spoon or a butter knife. Set aside.\n3. Slice the 4 ounces of cheddar cheese into long pieces. Stuff individual slices of cheese into each pepper. Scoop the cooked meat mixture and stuff into the chili peppers. Make sure that there is enough meat.\n4. Wrap the stuffed peppers in a lumpia wrapper.\n5. Heat 2 cups of oil in a pan. Fry each piece of dynamite lumpia in medium heat for 2 minutes per side or until the lumpia wrapper turns golden brown. Note: you may fry longer if needed.\n6. Remove from the pan and place over a wire rack. Let it cool down. Arrange on a serving plate and then serve with your favorite condiment.\n7. Share and enjoy!",
+    image: "Dynamite_Lumpia.jpg"
+  },
+  
+  {
+    id: 6,
+    name: "Bagoong Rice",
+    ingredients: [
+      "Pork belly",
+      "Scallions",
+      "Green mango",
+      "Steamed white rice",
+      "Bagoong guisado",
+      "Garlic minced",
+      "Cooking oil"
+    ],
+    procedure: "1. Heat a frying pan.\n2. Sear the sliced 1/4 lb pork belly by putting them directly in the hot pan and cooking them until oil comes out. This should take about 2 to 5 minutes.\n3. Add 1 tablespoon of bagoong guisado and then stir. Cook for a minute and then remove from the pan and set aside.\n4. Pour the 1 tablespoon cooking oil in the same frying pan.\n5. When the oil becomes hot, put in 1 teaspoon of garlic and then cook for 15 seconds.\n6. Add 2 tablespoons of shrimp paste and then cook for 30 seconds on medium heat.\n7. Put in the rice. Gently mix while cooking until all the ingredients are distributed properly. Cook the rice for 10 minutes while gently scraping the bottom of the pan once in a while.\n8. Transfer the rice to a serving bowl. Top with the cooked pork belly, 1/2 cup chopped green mango, and 3 tablespoons chopped scallions.\n9. Serve. Share and enjoy!",
+    image: "Bagoong_Rice.jpg"
+  },
+  
+  {
+    id: 7,
+    name: "Tokneneng",
+    ingredients: [
+      "Chicken eggs",
+      "Cornstarch",
+      "Water",
+      "Annatto seeds",
+      "All-purpose flour",
+      "Salt",
+      "Ground black pepper",
+      "Cooking oil"
+    ],
+    procedure: "1. Place the 3 tbsp cornstarch in a container and dredge the boiled quail eggs. Set aside.\n2. Combine all-purpose flour, 1/2 tsp salt, and 1/2 tsp pepper then mix thoroughly.\n3. Dilute the 1/4 cup annatto powder in warm water then pour-in the mixing bowl with the other ingredients. Mix well.\n4. Place all the quail eggs in the mixing bowl and coat with the batter.\n5. Put the boiled chicken eggs in the mixing bowl and coat with the batter.\n6. Heat the pan and pour 2 cups of cooking oil.\n7. When the oil is hot enough, deep-fry the eggs until the coating is crispy. Use a serving spoon to scoop the eggs from the mixing bowl.\n8. Remove the fried eggs from the pan and place in a serving plate.\n9. Serve with vinegar or special sauce.",
+    image: "Tokneneng.jpg"
+  },
+  
+  {
+    id: 8,
+    name: "Skinless Longganisa",
+    ingredients: [
+      "Ground pork",
+      "Dark brown sugar",
+      "Smoked paprika",
+      "Vegetable oil",
+      "Coarse salt",
+      "Black pepper",
+      "Head garlic"
+    ],
+    procedure: "1. Peel the skin off the garlic cloves. Crush thoroughly using mortar and pestle. Mince the crushed garlic. Set aside.\n2. In a large mixing bowl, combine ground pork along with all of the ingredients. Mix well.\n3. Cover the bowl. Refrigerate for 2 hours.\n4. Shape the longganisa by laying a sheet of wax paper on a flat surface. Scoop around 3 tablespoons of mixture and put over the wax paper. Fold the wax paper from top to down until the mixture is covered. Hold the edge of the paper with your fingers and then slide the card towards the mixture. Push a bit more until a sausage shape is formed. Do this step until the entire mixture is consumed.\n5. Refrigerate overnight.\n6. Cook and serve for breakfast. Share and enjoy!",
+    image: "Skinless_Longganisa.jpg"
+  },
+  
+  {
+    id: 9,
+    name: "Okoy",
+    ingredients: [
+      "Small shrimps cleaned",
+      "Cornstarch",
+      "Flour",
+      "Mung bean sprouts",
+      "Salt",
+      "Black pepper",
+      "Raw egg",
+      "Baking powder",
+      "Cups water",
+      "Cooking oil"
+    ],
+    "procedure": "1. Combine cornstarch, 1/4 flour, 1/4 teaspoon baking powder, 1 piece raw egg, 1 1/4 water, 2 teaspoons salt, and 1/2 teaspoon ground black pepper then stir until diluted. \n 2. Add the 1 cup small shrimps cleaned and 1 1/2 cups mung bean sprouts in the mixture and lightly stir to distribute.\n 3. Heat a frying pan or small cooking pot and pour-in the 2 cups of cooking oil. \n 4. When the oil is hot enough, scoop about 2 to 3 tablespoons of the mixture and quickly (but cautiously) drop it in hot oil. \n 5. Cook each side for 2 to 3 minutes or until the color turns golden brown. Please note that cooking time may vary. Repeat this step until the entire mixture is consumed. \n 6. Remove the deep-fried ukoy from the pan and place in a plate lined with a paper towel to absorb the oil. \n 7. Remove the paper towel and serve with sinamak. \n 8. Share and enjoy!",
+    image: "Okoy.jpg"
+  },
+  
+  {
+    id: 10,
+    name: "Tokwa at Baboy",
+    ingredients: [
+      "Pig ears",
+      "Pork belly",
+      "Tofu",
+      "White vinegar",
+      "Soy sauce",
+      "Sugar",
+      "Salt",
+      "Peppercorn",
+      "Onion",
+      "Scallions",
+      "Ground black pepper",
+      "Water",
+      "Cooking oil"
+    ],
+    "procedure": "1. Pour-in water in a pot and bring to a boil\n 2. Add salt and 1 whole peppercorn \n 3. Put-in the 1lb pig’s ears and 1 lb pork belly then simmer until tender (about 30 mins to 1 hour) \n 4. Pour 2 cups of cooking oil on a separate pan and allow to heat. \n 5. When the oil is hot enough, deep-fry the 1 lb tofu until the color turns golden brown and the outer texture is somewhat crispy  \n 6. Cube the fried tofu and slice the boiled pig’s ears and pork belly into bite-sized pieces then set aside \n 7. Combine the 1 tablespoon sugar, 1 tablespoon salt, 1/4 cup soy sauce, and 1 1/2 white vinegar in a bowl. Stir. \n 8. Microwave for 1 minute. \n 9. Add the 1/8 teaspoon ground black pepper, 1 piece onions, green onions, and chili pepper. Transfer the sauce in a serving bowl. \n 10. Place the sliced meat and tofu in a serving plate with the bowl of sauce by the side. \n 11. Serve hot! Share and Enjoy!",
+    image: "Tokwa_at_Baboy.jpg"
+  },
+  
+  {
+    id: 11,
+    name: "Laing",
+    ingredients: [
+      "Dried taro leaves",
+      "Pork belly",
+      "Salted dried fish daing, shredded",
+      "Coconut milk",
+      "Coconut cream",
+      "Thai chili",
+      "Onion sliced",
+      "Garlic crushed",
+      "Ginger sliced",
+      "Green chili",
+      "Salt and pepper to taste"
+    ],
+    "procedure": "1. Combine 1/2 lb pork thinly sliced, 3/4 cup salted dried fish, 5 cloves garlic, 1 medium onion, 2 tablespoons ginger,2 cups coconut milk, and 4 cups coconut cream in a cooking pot. \n 2. Apply heat and let boil. \n 3. Add the 1 pack of 4 ounces dried taro leaves and 15 pieces Thai chili (do not stir). Simmer until the liquid almost dries out. Note: This will take about 40 to 50 minutes. When you notice a strong scent of coconut, this can be a sign that its ready.  \n 4. Put in the 2 pieces long  long green chili, salt, and pepper. Gently stir the ingredients and cook for another 3 minutes.  \n 5. Transfer to a serving plate.\n 6. Serve. Share and enjoy",
+    image: "Laing.jpg"
+  },
+  
+  {
+    id: 12,
+    name: "Tortang Talong",
+    ingredients: [
+      "Eggplant",
+      "Raw eggs",
+      "Salt",
+      "Cooking oil"
+    ],
+    "procedure": "1. Grill the 4 pieces of eggplant until the color of skin turns almost black. \n 2. Let the eggplant cool for a while then peel off the skin. Set aside. \n 3. Crack the 2 pieces of eggs and place in a bowl \n 4. Add 1 teaspoon salt and beat \n 5. Place the eggplant on a flat surface and flatten using a fork. \n 6. Dip the flattened eggplant in the beaten egg mixture\n 7. Heat the pan and pour the 6 tablespoons of cooking oil  \n 8. Fry the eggplant (that was dipped in the beaten mixture). Make sure that both sides are cooked. Frying time will take you about 3 to 4 minutes per side on medium heat.",
+    image: "Torta_Talong.jpg"
+  },
+  
+  {
+    id: 13,
+    name: "Pinakbet",
+    ingredients: [
+      "Liempo",
+      "Knorr Pork Cube",
+      "Ube",
+      "Kalabasa",
+      "Sitaw sliced",
+      "Okra",
+      "Talong sliced",
+      "Ampalaya",
+      "Tomato",
+      "Onion",
+      "Ginger",
+      "Garlic chopped",
+      "Bagoong alamang",
+      "Water",
+      "Cooking oil",
+      "Ground black pepper to taste"
+    ],
+    "procedure": "1. Boil 5 cups of water in a cooking pot.\n2. 2. Add 1 1/2 lbs  pork belly. Continue boiling for 35 minutes. \n3. 3.Remove pork from the pot. Let it cool down. Note: Save the pork stock (water used to boil the pork). It will be used later. \n4. 4.Rub salt all over the pork belly. Let it stay for 10 minutes. \n5. 6.Put the pork in an air fryer. Air-fry for 18 minutes (or until crispy) per side at 370F. Slice into serving pieces. Set aside. \n6. 6. Heat 3 tablespoons of oil in a pan. Saute 1 piece chopped onion, 3 thumbs crushed garlic, 4 crushed ginger, and 1 piece cubed Tomato. \n7. 7. Once tomato and onion softens, add 1 piece  ube and 1 1/2 cups cubed kalabasa. Saute for 2 minutes. \n8. 8. Pour 1 1/4 cup pork stock into the pot. Let boil. \n9. 9. Add 1 piece 1 piece Knorr Pork Cube. Stir. \n10. 10. Add 3 tablespoons  bagoong alamang. Stir. Cover the pan. Cook for 5 minutes. \n11. 11. Add1 piece eggplant sliced and 1 piece  ampalaya. Cover and cook for 2 minutes. \n12.  12. Add  10 pieces of okra and snake beans. Cover and cook for 7 to 10 minutes. \n13. 13. Season with ground black pepper. Top with crispy liempo. Serve. Share and enjoy.",
+    "image": "Pinakbet.jpg"
+  },
+  
+  {
+    id: 14,
+    name: "Pork Barbeque",
+    ingredients: [
+      "Pork",
+      "3 tablespoons Knorr Liquid Seasoning",
+      "Calamansi",
+      "Banana ketchup",
+      "Soy sauce",
+      "Brown sugar",
+      "Garlic powder",
+      "Soy sauce",
+      "Knorr Liquid Seasoning",
+      "Calamansi",
+      "Vegetable oil"
+    ],
+    "procedure": "1. Place the 3 lbs pork shoulder sliced into thin pieces in a large bowl. Add all the BBQ marinade ingredients. Mix well. Cover the bowl and place inside the refrigerator. Marinate for at least 3 hours. Note: marinating for a longer period of time is ideal for the meat to absorb the flavors from the marinade. \n2. 2. Skewer the marinated pork in bamboo skewers.  \n3. 3. Prepare the basting sauce by combining all the basting sauce ingredients in a bowl. Mix well and set aside. \n4. 4. Heat-up the grill. Grill the pork barbeque for 2 minutes. Baste the top part using a brush and then turn it over. Baste the top again. Continue to grill one side for 2 to 2 1 ⁄ 2 minutes while basting every time its flipped until the BBQ is done. \n5. 5. Arrange on a serving plate. Serve with rice on the side and a dipping sauce of spicy vinegar. Share and enjoy!",
+    image: "Pork_Barbeque.jpg"
+  },
+  
+
+  
     ];
