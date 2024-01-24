1adb80f1228cc7dc4d6c1d8bd1a82f53a893aac6
G_Rodolfo <girondudut92@gmail.com>
2024-01-24 11:29:56 +0800

    Lechon kawali
    added lechon kawali recipe in the data recipe

--- cuisinecode/src/components/RecipeData.js
+++ cuisinecode/src/components/RecipeData.js
@@ -25,4 +25,60 @@ export const recipes = [
     procedure: "1. Heat 4 tablespoons of oil in a cooking pot. Saute onion and garlic until the onion softens.\n2. Add 8 ounces of pork belly and sausage. Saute for 3 minutes.\n3. Pour soy sauce and water. Let boil.\n4. Add 2 pieces of Knorr Pork cube. Stir and cook for 1 minute.\n5. Add 24 ounces of bihon noodles. Cover the pot and then continue cooking until the noodles start to soften.\n6. Add 1/2 cup of oyster sauce. Toss until all ingredients are well distributed.\n7. Put in 1/4 cup of dried shrimp, cabbage, snap peas, 1 1/2 carrots. Toss. Cook for 7 minutes.\n8. Season with ground black pepper and 1 cup soy sauce if needed.\n9. Arrange in a serving tray and top with chopped lechon kawali.\n10. Serve with sliced lime or calamansi. Share and enjoy!",
     image: "Pancit_Bihon_with_lechon.jpg" 
   },
+  {
+    id: 2,
+    name: "Pork Adobo",
+    ingredients: [
+      "Pork belly",
+      "Garlic",
+      "Dried bay leaves",
+      "Vinegar",
+      "Soy sauce",
+      "Peppercorn",
+      "Water",
+      "Salt to taste"
+    ],
+    procedure: "1. Combine the 2 lbs pork belly, 1/2 cup soy sauce, and 2 tablespoons garlic then marinade for at least 1 hour.\n2. Heat the pot and put-in the marinated pork belly then cook for a few minutes\n3. Pour remaining marinade including garlic.\n4. Add water, whole pepper corn, and 5 dried bay leaves then bring to a boil. Simmer for 40 minutes to 1 hour\n5. Put in the 4 tablespoons of vinegar and simmer for 12 to 15 minutes\n6. Add salt to taste.\n7. Serve hot. Share and enjoy",
+    image: "Pork_Adobo.jpeg"
+  },
+  {
+    id: 3,
+    name: "Chicharong Bulaklak",
+    ingredients: [
+      "Ruffle fat",
+      "Whole peppercorn",
+      "Dried bay leaves",
+      "Sea salt",
+      "canola oil",
+      "Water"
+    ],
+    procedure: "1. Arrange the 2 lbs of ruffle fat, 1 tablespoon of whole peppercorn, 2 tablespoons salt, and 5 bay leaves in a cooking pot. Pour-in 6 cups of water. Let boil. Cover and then simmer for 1 hour.\n2. Turn the heat off. Remove the boiled ruffle fat from the pot and then let the water drip-off.\n3. Rub the remaining salt all over the ruffled fat. Set aside.\n4. Meanwhile, heat the 4 cups of cooking oil in a deep fryer or a deep cooking pot.\n5. When the oil gets hot, slowly add-in the ruffled fat. Deep fry in low to medium heat until the texture becomes crisp and the color turns medium brown.\n6. Add salt to taste.\n7. Remove from the deep fryer or cooking pot. Arrange on a plate lined with paper towels to absorb the oil.\n8. Serve with spiced vinegar.",
+    image: "Chicharon_Bulaklak.jpeg"
+  },
+  {
+    id: 4,
+    name: "Lechon Kawali Sisig",
+    ingredients: [
+      "Lady’s Choice Mayonnaise",
+      "Tablespoons Knorr liquid seasoning",
+      "lemon lime soda",
+      "Calamansi juice",
+      "White vinegar",
+      "Ground black pepper",
+      "Pork belly",
+      "Dried bay leaves",
+      "Whole peppercorn",
+      "Liver spread",
+      "Chili pepper",
+      "Green onions",
+      "Red onions",
+      "Salt",
+      "Water",
+      "Cooking oil"
+    ],
+    procedure: "1. Combine all Sisig seasoning ingredients in a bowl. Mix well until smooth. Set aside.\n2. Make lechon kawali sisig by boiling the pork belly with whole peppercorn and dried bay leaves in 6 to 8 cups of water for 50 minutes or until tender\n3. Remove from the pot and let it cool down.\n4. Rub it with 2 teaspoons of salt. Deep-fry the pork belly until golden brown and crispy.\n5. Chop into serving pieces.\n6. Combine a quarter of the sisig seasoning ingredients with 2 tablespoons of liver spread.\n7. Put the chopped pork belly, Thai chili pepper, green onions, and red onions in a mixing bowl and then add the Sisig seasoning mixture.\n8. Toss until well blended. Serve.",
+    image: "Lechon_Kawali_Sisig"
+  },
+  
+
 ];
