f94032f809336528e34ccf14a85ffd7ebcb77a7b
katktrn <130741414+katktrn@users.noreply.github.com>
2024-01-24 21:39:34 +0800

    deleted some codes

--- cuisinecode/src/components/RandomRecipe.js
+++ cuisinecode/src/components/RandomRecipe.js
@@ -3,18 +3,18 @@ import { recipes } from './RecipeData';
 
 const RandomRecipe = ({ randomIndex, setRandomIndex }) => {
   useEffect(() => {
-    // Generate a random index only on component mount
+ 
     const newRandomIndex = Math.floor(Math.random() * recipes.length);
     setRandomIndex(newRandomIndex);
-  }, []); // Empty dependency array ensures the effect runs only once on mount
+  }, []); 
 
-  // Check if randomIndex is within valid range
+  
   const isValidIndex = randomIndex !== null && randomIndex >= 0 && randomIndex < recipes.length;
 
-  // Display the recipe using the random index
+ 
   const randomRecipe = isValidIndex ? recipes[randomIndex] : null;
 
-  // Function to generate a new random index and update the state
+  
   const generateNewRandomIndex = () => {
     const newRandomIndex = Math.floor(Math.random() * recipes.length);
     setRandomIndex(newRandomIndex);
