6d05345862e8110afd7b4ba2e799ad1b8f13af94
G_Rodolfo <girondudut92@gmail.com>
2024-01-24 11:32:05 +0800

    dynamite lumpia
    added the dynamite lumpia recipe to the data recipe

--- cuisinecode/src/components/RecipeData.js
+++ cuisinecode/src/components/RecipeData.js
@@ -53,7 +53,7 @@ export const recipes = [
       "Water"
     ],
     procedure: "1. Arrange the 2 lbs of ruffle fat, 1 tablespoon of whole peppercorn, 2 tablespoons salt, and 5 bay leaves in a cooking pot. Pour-in 6 cups of water. Let boil. Cover and then simmer for 1 hour.\n2. Turn the heat off. Remove the boiled ruffle fat from the pot and then let the water drip-off.\n3. Rub the remaining salt all over the ruffled fat. Set aside.\n4. Meanwhile, heat the 4 cups of cooking oil in a deep fryer or a deep cooking pot.\n5. When the oil gets hot, slowly add-in the ruffled fat. Deep fry in low to medium heat until the texture becomes crisp and the color turns medium brown.\n6. Add salt to taste.\n7. Remove from the deep fryer or cooking pot. Arrange on a plate lined with paper towels to absorb the oil.\n8. Serve with spiced vinegar.",
-    image: "Chicharon_Bulaklak.jpeg"
+    image: "Chicharon_Bulaklak.jpg"
   },
   {
     id: 4,
@@ -78,6 +78,24 @@ export const recipes = [
     ],
     procedure: "1. Combine all Sisig seasoning ingredients in a bowl. Mix well until smooth. Set aside.\n2. Make lechon kawali sisig by boiling the pork belly with whole peppercorn and dried bay leaves in 6 to 8 cups of water for 50 minutes or until tender\n3. Remove from the pot and let it cool down.\n4. Rub it with 2 teaspoons of salt. Deep-fry the pork belly until golden brown and crispy.\n5. Chop into serving pieces.\n6. Combine a quarter of the sisig seasoning ingredients with 2 tablespoons of liver spread.\n7. Put the chopped pork belly, Thai chili pepper, green onions, and red onions in a mixing bowl and then add the Sisig seasoning mixture.\n8. Toss until well blended. Serve.",
     image: "Lechon_Kawali_Sisig"
+  },
+  {
+    id: 5,
+    name: "Dynamite Lumpia",
+    ingredients: [
+      "Long green pepper",
+      "Cheddar cheese",
+      "Lumpia wrapper",
+      "Cooking oil",
+      "Ground pork",
+      "Onion minced",
+      "Cloves garlic crushed and minced",
+      "Egg",
+      "Cooking oil",
+      "Salt and ground black pepper to taste"
+    ],
+    procedure: "1. Prepare the ground pork stuffing by heating 3 tablespoons of oil in a pan. Saute garlic and onion until the latter softens. Add ground pork. Saute until medium brown. Season with salt and ground black pepper. Remove from the pan and put on a large bowl. Let it cool down. Beat 1 piece of egg and pour into the cooked ground pork. Mix well. Set aside.\n2. Slice one side of the peppers lengthwise all the way to the bottom. Remove the seeds by gently scraping using a small spoon or a butter knife. Set aside.\n3. Slice the 4 ounces of cheddar cheese into long pieces. Stuff individual slices of cheese into each pepper. Scoop the cooked meat mixture and stuff into the chili peppers. Make sure that there is enough meat.\n4. Wrap the stuffed peppers in a lumpia wrapper.\n5. Heat 2 cups of oil in a pan. Fry each piece of dynamite lumpia in medium heat for 2 minutes per side or until the lumpia wrapper turns golden brown. Note: you may fry longer if needed.\n6. Remove from the pan and place over a wire rack. Let it cool down. Arrange on a serving plate and then serve with your favorite condiment.\n7. Share and enjoy!",
+    image: "Dynamite_Lumpia.jpg"
   },
   
 
