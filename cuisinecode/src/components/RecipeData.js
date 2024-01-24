1463e8d7d64d4a4702cd19ff9866d973c89c8e69
G_Rodolfo <girondudut92@gmail.com>
2024-01-24 11:20:26 +0800

    Added a recipe
    added pancit bihon with lecon recipe and its picture

--- cuisinecode/src/components/RecipeData.js
+++ cuisinecode/src/components/RecipeData.js
@@ -1,10 +1,28 @@
 
 export const recipes = [
 {
-    id: 15,
-    name: "Chicken Inasal",
-    ingredients : ["chicken", "ginger", "lemongrass", "garlic", "vinegar", "lemon or calamansi juice","salt", "brown sugar", "lemon soda", "ground black pepper", "atsuete oil", "margarine"],
-    procedure: "1. In a freezer bag or large bowl, combine 2 lbs chicken cut into serving pieces, lemongrass, salt, ½ tablespoon ground black pepper, 2 tablespoons ginger, 2 tablespoons garlic, ¼ cup of brown sugar, 1 cup vinegar, 1 cup lemon-lime soda, and ½ cup lemon juice.\n2. 2. Stir or shake the mixture until every ingredient is well incorporated. Marinade for 1 to 3 hours.\n3. 3. In a bowl, combine margarine, annatto oil, salt, and lemon juice then stir. Set aside.\n4. 4. Grill the chicken while basting generous amounts of the margarine mixture.\n5. 5. Transfer the grilled chicken to a serving plate.\n6. 6. Serve with sinamak. Share and enjoy!",
-    image: "chicken_inasal.jpg"
+    id: 1,
+    name: "Pancit Bihon with Lechon",
+    ingredients: [
+      "Bihon",
+      "Knorr Pork Cube",
+      "Pork belly boiled and sliced",
+      "Lechon kawali",
+      "Cooking oil",
+      "Dried shrimp optional",
+      "Cups carrot",
+      "snap peas",
+      "cabbage, chopped",
+      "onion, chopped",
+      "Garlic, chopped",
+      "Chinese sausage, sliced",
+      "oyster sauce",
+      "Soy sauce",
+      "Cups water",
+      "Parsley, chopped",
+      "Ground black pepper to taste" ],
+  
+    procedure: "1. Heat 4 tablespoons of oil in a cooking pot. Saute onion and garlic until the onion softens.\n2. Add 8 ounces of pork belly and sausage. Saute for 3 minutes.\n3. Pour soy sauce and water. Let boil.\n4. Add 2 pieces of Knorr Pork cube. Stir and cook for 1 minute.\n5. Add 24 ounces of bihon noodles. Cover the pot and then continue cooking until the noodles start to soften.\n6. Add 1/2 cup of oyster sauce. Toss until all ingredients are well distributed.\n7. Put in 1/4 cup of dried shrimp, cabbage, snap peas, 1 1/2 carrots. Toss. Cook for 7 minutes.\n8. Season with ground black pepper and 1 cup soy sauce if needed.\n9. Arrange in a serving tray and top with chopped lechon kawali.\n10. Serve with sliced lime or calamansi. Share and enjoy!",
+    image: "Pancit_Bihon_with_lechon.jpg" 
   },
 ];
